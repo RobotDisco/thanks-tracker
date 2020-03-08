1985ec7371303fe1fca97ab5ac204382c71f27c0
Gaelan D'costa <gdcosta@gmail.com>
2020-03-08 16:11:31 -0400

    Unit tests for creating user, kudos, and adding kudos to user.

--- src/thanks-tracker.lisp
+++ src/thanks-tracker.lisp
@@ -1,23 +1,35 @@
 (defpackage thanks-tracker
   (:use :cl)
   (:export :make-doc
+	   :make-kudos
 	   :thankee
 	   :kudos
-	   :-main))
+	   :kudos-thanker
+	   :kudos-message
+	   :kudos-date
+	   :add-kudos))
 
 (in-package :thanks-tracker)
-
-(defun -main (&optional args)
-  (format t "~a~%" "I dont do much yet."))
 
 (defparameter gaelan-doc '((_id . "gaelan@tulip.com")
 			   (kudos . (((_id . "gaelan@tulip.com")
 				      (message . "for doing absolutely nothing.")
 				      (date . (2020 3 7)))))))
 
+
+
 (defun make-doc (id)
   (list (cons '_id id)
 	(cons 'kudos ())))
+
+(defun make-kudos (thanker msg date)
+  (list (cons '_id thanker)
+	(cons 'message msg)
+	(cons 'date date)))
+
+(defun add-kudos (doc entry)
+  (let ((newkudos (cons entry (kudos doc))))
+    (acons 'kudos newkudos doc)))
 
 (defun thankee (doc)
   (cdr (assoc '_id doc)))
