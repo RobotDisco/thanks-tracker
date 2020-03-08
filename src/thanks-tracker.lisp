d11a33c4b1f391f66cc8090cf055cd9e07cfa8d1
Gaelan D'costa <gdcosta@gmail.com>
2020-03-08 10:31:12 -0400

    Sketching out a simple association list as kudos data structure.

--- src/thanks-tracker.lisp
+++ src/thanks-tracker.lisp
@@ -7,3 +7,24 @@
 (defun -main (&optional args)
   (format t "~a~%" "I dont do much yet."))
 
+(defparameter gaelan-doc '((_id . "gaelan@tulip.com")
+			   (kudos . (((_id . "gaelan@tulip.com")
+				      (message . "for doing absolutely nothing.")
+				      (date . (2020 3 7)))))))
+
+(defun thankee (doc)
+  (cdr (assoc '_id doc)))
+
+(defun kudos (doc)
+  (cdr (assoc 'kudos doc)))
+
+;; TODO find a reasonable way to map two symbols to the same lambda (thankee and kudos-thanker are the same function right now)
+(defun kudos-thanker (doc)
+  (thankee doc))
+
+(defun kudos-message (doc)
+  (cdr (assoc 'message doc)))
+
+(defun kudos-date (doc)
+  (cdr (assoc 'date doc)))
+
